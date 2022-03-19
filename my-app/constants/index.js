dc06dd192b071830e9e8880592d2d41cf2eb3f6f
graphica99 <mendsalbert@gmail.com>
2022-03-19 09:12:51 +0000

    crypto dev

--- my-app/constants/index.js
+++ my-app/constants/index.js
@@ -1,4 +1,6 @@
-export const abi = "../artifacts/contracts/CryptoDevs.sol/CryptoDevs.json";
+export const abi = "./artifacts/contracts/CryptoDevs.sol/CryptoDevs.json";
+
+// import '../../artifacts/contracts/CryptoDevs.sol/CryptoDevs.json'
 //   "../../../artifacts/contracts/CryptoDevs.sol/CryptoDevs.json";
 export const NFT_CONTRACT_ADDRESS =
   "0x7Aa4a7D2b7ea1B113656B6c0755bb5030BB75896";
