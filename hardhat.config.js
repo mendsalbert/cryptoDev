c08eacc15735bdb41d1d867215a7609064c56da8
graphica99 <mendsalbert@gmail.com>
2022-03-19 07:16:22 +0000

    crypto dev

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,33 +1,16 @@
-/* hardhat.config.js */
 require("@nomiclabs/hardhat-waffle");
-const fs = require("fs");
-const privateKey =
-  "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
-const projectId = "8b7ba5517c414450a93ec7334975a7fe";
+require("dotenv").config({ path: ".env" });
+
+const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
+
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
+
 module.exports = {
-  defaultNetwork: "hardhat",
+  solidity: "0.8.4",
   networks: {
-    hardhat: {
-      chainId: 1337,
-    },
-    //  unused configuration commented out for now
-    mumbai: {
-      url: "https://rpc-mumbai.maticvigil.com",
-      // url: "https://goerli.infura.io/v3/8b7ba5517c414450a93ec7334975a7fe",
-      accounts: [privateKey],
-    },
-    mainet: {
-      url: `https://palm-mainnet.infura.io/v3/${projectId}`,
-      accounts: [privateKey],
-    },
-  },
-  solidity: {
-    version: "0.8.4",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
+    rinkeby: {
+      url: ALCHEMY_API_KEY_URL,
+      accounts: [RINKEBY_PRIVATE_KEY],
     },
   },
 };
