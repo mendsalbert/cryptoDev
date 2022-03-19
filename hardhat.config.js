08bed59e2f9ac1d7c11a1ec0d22f3b5f48bd3e82
graphica99 <mendsalbert@gmail.com>
2022-03-19 19:55:36 +0000

    crypto dev

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
 require("dotenv").config({ path: ".env" });
+require("@nomiclabs/hardhat-etherscan");
 
 const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
 
