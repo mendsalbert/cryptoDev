feeceee53ed1e786144a96ab6e0ad50b910f90ea
graphica99 <mendsalbert@gmail.com>
2022-03-19 21:21:22 +0000

    crypto dev

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,35 +1,22 @@
 const { ethers } = require("hardhat");
 require("dotenv").config({ path: ".env" });
-require("@nomiclabs/hardhat-etherscan");
 
 async function main() {
+  // URL from where we can extract the metadata for a LW3Punks
+  const metadataURL = "ipfs://Qmbygo38DWF1V8GttM1zy89KzyZTPU2FLUzQtiDvB7q6i5/";
   /*
   A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
-  so verifyContract here is a factory for instances of our Verify contract.
+  so lw3PunksContract here is a factory for instances of our LW3Punks contract.
   */
-  const verifyContract = await ethers.getContractFactory("Verify");
+  const lw3PunksContract = await ethers.getContractFactory("LW3Punks");
 
   // deploy the contract
-  const deployedVerifyContract = await verifyContract.deploy();
+  const deployedLW3PunksContract = await lw3PunksContract.deploy(metadataURL);
 
-  await deployedVerifyContract.deployed();
+  await deployedLW3PunksContract.deployed();
 
   // print the address of the deployed contract
-  console.log("Verify Contract Address:", deployedVerifyContract.address);
-
-  console.log("Sleeping.....");
-  // Wait for etherscan to notice that the contract has been deployed
-  await sleep(50000);
-
-  // Verify the contract after deploying
-  await hre.run("verify:verify", {
-    address: deployedVerifyContract.address,
-    constructorArguments: [],
-  });
-}
-
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+  console.log("LW3Punks Contract Address:", deployedLW3PunksContract.address);
 }
 
 // Call the main function and catch if there is any error
