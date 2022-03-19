19c9e66cac40fea8fb994cadbef3a240a993391b
graphica99 <mendsalbert@gmail.com>
2022-03-19 10:46:29 +0000

    crypto dev

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,32 +1,32 @@
 const { ethers } = require("hardhat");
-require("dotenv").config({ path: ".env" });
-const { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS } = require("../constants");
+const { CRYPTODEVS_NFT_CONTRACT_ADDRESS } = require("../constants");
 
 async function main() {
-  // Address of the Crypto Devs NFT contract that you deployed in the previous module
-  const cryptoDevsNFTContract = CRYPTO_DEVS_NFT_CONTRACT_ADDRESS;
-
-  /*
-    A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
-    so cryptoDevsTokenContract here is a factory for instances of our CryptoDevToken contract.
-    */
-  const cryptoDevsTokenContract = await ethers.getContractFactory(
-    "CryptoDevToken"
+  // Deploy the FakeNFTMarketplace contract first
+  const FakeNFTMarketplace = await ethers.getContractFactory(
+    "FakeNFTMarketplace"
   );
+  const fakeNftMarketplace = await FakeNFTMarketplace.deploy();
+  await fakeNftMarketplace.deployed();
 
-  // deploy the contract
-  const deployedCryptoDevsTokenContract = await cryptoDevsTokenContract.deploy(
-    cryptoDevsNFTContract
-  );
+  console.log("FakeNFTMarketplace deployed to: ", fakeNftMarketplace.address);
 
-  // print the address of the deployed contract
-  console.log(
-    "Crypto Devs Token Contract Address:",
-    deployedCryptoDevsTokenContract.address
+  // Now deploy the CryptoDevsDAO contract
+  const CryptoDevsDAO = await ethers.getContractFactory("CryptoDevsDAO");
+  const cryptoDevsDAO = await CryptoDevsDAO.deploy(
+    fakeNftMarketplace.address,
+    CRYPTODEVS_NFT_CONTRACT_ADDRESS,
+    {
+      // This assumes your account has at least 1 ETH in it's account
+      // Change this value as you want
+      value: ethers.utils.parseEther("1"),
+    }
   );
+  await cryptoDevsDAO.deployed();
+
+  console.log("CryptoDevsDAO deployed to: ", cryptoDevsDAO.address);
 }
 
-// Call the main function and catch if there is any error
 main()
   .then(() => process.exit(0))
   .catch((error) => {
