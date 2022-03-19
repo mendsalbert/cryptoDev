755efe4f4a7fc0769d1c16789f27f5974e087d7d
graphica99 <mendsalbert@gmail.com>
2022-03-19 12:21:22 +0000

    crypto dev

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -287,10 +287,10 @@ export default function Home() {
 
     // If user is not connected to the Rinkeby network, let them know and throw an error
     const { chainId } = await web3Provider.getNetwork();
-    if (chainId !== 4) {
-      window.alert("Change the network to Rinkeby");
-      throw new Error("Change network to Rinkeby");
-    }
+    // if (chainId !== 4) {
+    //   window.alert("Change the network to Rinkeby");
+    //   throw new Error("Change network to Rinkeby");
+    // }
 
     if (needSigner) {
       const signer = web3Provider.getSigner();
