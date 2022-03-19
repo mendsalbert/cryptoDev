92b957129f57f415a866dad781733e003418af10
graphica99 <mendsalbert@gmail.com>
2022-03-19 10:36:27 +0000

    crypto dev

--- contracts/CryptoDevsDAO.sol
+++ contracts/CryptoDevsDAO.sol
@@ -2,9 +2,115 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+/**
+ * Interface for the FakeNFTMarketplace
+ */
 
-// We will add the Interfaces here
+interface IFakeNFTMarketplace {
+    /// @dev getPrice() returns the price of an NFT from the FakeNFTMarketplace
+    /// @return Returns the price in Wei for an NFT
+    function getPrice() external view returns (uint256);
+
+    /// @dev available() returns whether or not the given _tokenId has already been purchased
+    /// @return Returns a boolean value - true if available, false if not
+    function available(uint256 _tokenId) external view returns (bool);
+
+    /// @dev purchase() purchases an NFT from the FakeNFTMarketplace
+    /// @param _tokenId - the fake NFT tokenID to purchase
+    function purchase(uint256 _tokenId) external payable;
+}
+
+/**
+ * Minimal interface for CryptoDevsNFT containing only two functions
+ * that we are interested in
+ */
+interface ICryptoDevsNFT {
+    /// @dev balanceOf returns the number of NFTs owned by the given address
+    /// @param owner - address to fetch number of NFTs for
+    /// @return Returns the number of NFTs owned
+    function balanceOf(address owner) external view returns (uint256);
+
+    /// @dev tokenOfOwnerByIndex returns a tokenID at given index for owner
+    /// @param owner - address to fetch the NFT TokenID for
+    /// @param index - index of NFT in owned tokens array to fetch
+    /// @return Returns the TokenID of the NFT
+    function tokenOfOwnerByIndex(address owner, uint256 index)
+        external
+        view
+        returns (uint256);
+}
+
+
+
+
 
 contract CryptoDevsDAO is Ownable {
+
+ 
+
+
+struct Proposal {
+    // nftTokenId - the tokenID of the NFT to purchase from FakeNFTMarketplace if the proposal passes
+    uint256 nftTokenId;
+    // deadline - the UNIX timestamp until which this proposal is active. Proposal can be executed after the deadline has been exceeded.
+    uint256 deadline;
+    // yayVotes - number of yay votes for this proposal
+    uint256 yayVotes;
+    // nayVotes - number of nay votes for this proposal
+    uint256 nayVotes;
+    // executed - whether or not this proposal has been executed yet. Cannot be executed before the deadline has been exceeded.
+    bool executed;
+    // voters - a mapping of CryptoDevsNFT tokenIDs to booleans indicating whether that NFT has already been used to cast a vote or not
+    mapping(uint256 => bool) voters;
+}
+
+
+mapping(uint256 => Proposal) public proposals;
+// Number of proposals that have been created
+uint256 public numProposals;
+
+IFakeNFTMarketplace nftMarketplace;
+ICryptoDevsNFT cryptoDevsNFT;
+
+constructor(address _nftMarketplace, address _cryptoDevsNFT) payable {
+    nftMarketplace = IFakeNFTMarketplace(_nftMarketplace);
+    cryptoDevsNFT = ICryptoDevsNFT(_cryptoDevsNFT);
+}
+
+
+modifier nftHolderOnly() {
+    require(cryptoDevsNFT.balanceOf(msg.sender) > 0, "NOT_A_DAO_MEMBER");
+    _;
+}
+
+
+function createProposal(uint256 _nftTokenId)
+    external
+    nftHolderOnly
+    returns (uint256)
+{
+    require(nftMarketplace.available(_nftTokenId), "NFT_NOT_FOR_SALE");
+    Proposal storage proposal = proposals[numProposals];
+    proposal.nftTokenId = _nftTokenId;
+    // Set the proposal's voting deadline to be (current time + 5 minutes)
+    proposal.deadline = block.timestamp + 5 minutes;
+
+    numProposals++;
+
+    return numProposals - 1;
+}
+
+modifier activeProposalOnly(uint256 proposalIndex) {
+    require(
+        proposals[proposalIndex].deadline > block.timestamp,
+        "DEADLINE_EXCEEDED"
+    );
+    _;
+}
+
+enum Vote {
+    YAY,
+    NAY 
+}
     // We will write contract code here
 }
