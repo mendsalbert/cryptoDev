bd74942a3ccf4b05be2184487d155e61e28be4d7
graphica99 <mendsalbert@gmail.com>
2022-03-19 08:37:33 +0000

    crypto dev

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -231,10 +231,10 @@ export default function Home() {
 
     // If user is not connected to the Rinkeby network, let them know and throw an error
     const { chainId } = await web3Provider.getNetwork();
-    if (chainId !== 4) {
-      window.alert("Change the network to Rinkeby");
-      throw new Error("Change network to Rinkeby");
-    }
+    // if (chainId !== 4) {
+    //   window.alert("Change the network to Rinkeby");
+    //   throw new Error("Change network to Rinkeby");
+    // }
 
     if (needSigner) {
       const signer = web3Provider.getSigner();
@@ -252,7 +252,7 @@ export default function Home() {
       // Assign the Web3Modal class to the reference object by setting it's `current` value
       // The `current` value is persisted throughout as long as this page is open
       web3ModalRef.current = new Web3Modal({
-        network: "rinkeby",
+        network: "",
         providerOptions: {},
         disableInjectedProvider: false,
       });
