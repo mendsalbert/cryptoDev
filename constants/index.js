4d274fa179a91706aacced2b0a1d07a4f80a1307
graphica99 <mendsalbert@gmail.com>
2022-03-19 08:39:37 +0000

    crypto dev

--- constants/index.js
+++ constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x862B7a6DE5Fa22Fc7d9F2c3b5815fEAfCc59f8B9";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "http://localhost:3000/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
