b473eed9dd9389c5afaef900a5f7df81b9bba298
graphica99 <mendsalbert@gmail.com>
2022-03-19 22:11:25 +0000

    crypto dev

--- ceramic/pages/index.js
+++ ceramic/pages/index.js
@@ -4,14 +4,26 @@ import styles from "../styles/Home.module.css";
 import { Web3Provider } from "@ethersproject/providers";
 import { useEffect, useRef, useState } from "react";
 import Web3Modal from "web3modal";
+import { useViewerConnection } from "@self.id/react";
 export default function Home() {
   const web3ModalRef = useRef();
-
+  const [connection, connect, disconnect] = useViewerConnection();
   const getProvider = async () => {
     const provider = await web3ModalRef.current.connect();
     const wrappedProvider = new Web3Provider(provider);
     return wrappedProvider;
   };
+
+  useEffect(() => {
+    if (connection.status !== "connected") {
+      web3ModalRef.current = new Web3Modal({
+        network: "",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+    }
+  }, [connection.status]);
+
   return (
     <div className={styles.container}>
       <Head>
