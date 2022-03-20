cc7fd946001f3628fa6727b7d57d919aa9aa7e68
graphica99 <mendsalbert@gmail.com>
2022-03-20 18:56:41 +0000

    crypto dev

--- hardhat-tutorial/hardhat.config.js
+++ hardhat-tutorial/hardhat.config.js
@@ -1,21 +1,21 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config({ path: ".env" });
+require("@nomiclabs/hardhat-etherscan");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [
+        "7a7af74f631ff89ba62c48d8029ca2b353fc5b384558ff47f1ad9ba5c1d52ccf",
+      ],
+    },
+  },
+
+  etherscan: {
+    apiKey: {
+      polygonMumbai: "7EYZVIBKUI3WF1Q7XG5PP34VZSH5IQ1SX3",
+    },
+  },
 };
