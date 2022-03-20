560b1b6b26d362a784c564e2da79fa7a79f68bcd
graphica99 <mendsalbert@gmail.com>
2022-03-20 16:21:58 +0000

    crypto dev

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,22 +1,43 @@
 const { ethers } = require("hardhat");
 require("dotenv").config({ path: ".env" });
+require("@nomiclabs/hardhat-etherscan");
+const { FEE, VRF_COORDINATOR, LINK_TOKEN, KEY_HASH } = require("../constants");
 
 async function main() {
-  // URL from where we can extract the metadata for a LW3Punks
-  const metadataURL = "ipfs://Qmbygo38DWF1V8GttM1zy89KzyZTPU2FLUzQtiDvB7q6i5/";
   /*
-  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
-  so lw3PunksContract here is a factory for instances of our LW3Punks contract.
-  */
-  const lw3PunksContract = await ethers.getContractFactory("LW3Punks");
-
+ A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+ so randomWinnerGame here is a factory for instances of our RandomWinnerGame contract.
+ */
+  const randomWinnerGame = await ethers.getContractFactory("RandomWinnerGame");
   // deploy the contract
-  const deployedLW3PunksContract = await lw3PunksContract.deploy(metadataURL);
+  const deployedRandomWinnerGameContract = await randomWinnerGame.deploy(
+    VRF_COORDINATOR,
+    LINK_TOKEN,
+    KEY_HASH,
+    FEE
+  );
 
-  await deployedLW3PunksContract.deployed();
+  await deployedRandomWinnerGameContract.deployed();
 
   // print the address of the deployed contract
-  console.log("LW3Punks Contract Address:", deployedLW3PunksContract.address);
+  console.log(
+    "Verify Contract Address:",
+    deployedRandomWinnerGameContract.address
+  );
+
+  console.log("Sleeping.....");
+  // Wait for etherscan to notice that the contract has been deployed
+  await sleep(50000);
+
+  // Verify the contract after deploying
+  await hre.run("verify:verify", {
+    address: deployedRandomWinnerGameContract.address,
+    constructorArguments: [VRF_COORDINATOR, LINK_TOKEN, KEY_HASH, FEE],
+  });
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 // Call the main function and catch if there is any error
